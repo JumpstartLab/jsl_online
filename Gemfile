c7c73597b9eda4fe3babd409d6362be51b32807c
novohispano <novohispano@gmail.com>
2013-09-18 08:45:51 -0600

    Project Setup

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.0'
+gem 'less-rails'
+gem 'therubyracer'
+gem 'less-rails-bootstrap'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'pg'
+gem 'haml'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
+group :test do
+  gem 'rspec-rails'
+  gem 'simplecov'
+  gem 'guard'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JslOnline::Application.config.secret_key_base = 'd5c850b9baf939990a368c681b511eb716b12327d809c4b118294aaf14c7ccd1145cc1d4aa498d2ad39cf1c5fba8f404c01600897d506ffbfc6d5fa8d85a5671'
